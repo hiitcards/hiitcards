6d90dcd5a67844a9a93eda1db2abb5ea4787e46a
kahneraja <kahneraja@gmail.com>
2018-05-12 08:26:40 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          <HiitCard></HiitCard>
-        </p>
+        <HiitCard image="matthew.png" name="Matthew" description="Matthew is a musician living in Nashville."></HiitCard>
       </div>
     );
   }
