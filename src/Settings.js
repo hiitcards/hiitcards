80903408d38750aac1f7a5d7c7d70beb32333a13
kahneraja <kahneraja@gmail.com>
2018-08-14 06:40:56 -0400

    wip

--- src/Settings.js
+++ src/Settings.js
@@ -78,7 +78,7 @@ class Settings extends Component {
         </Form.Field>
         <Button onClick={this.onSubmit}>Done</Button>
         <Segment basic className="right aligned">
-          <div className="">v1.1.8</div>
+          <div className="">v1.1.9</div>
           <div><a href="http://www.kahneraja.com">@kahneraja</a></div>
         </Segment>
       </Form>
